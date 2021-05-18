71084a3162eb9c525121185a97b52be498da5076
Mamashov Aman <aman.mamashovfend@gmail.com>
2021-05-18 21:17:51 +0600

    save now

--- js.js
+++
@@ -1,8 +0,0 @@
-function local(){
-    var lang=document.getElementById('local')
-    var menu=document.getElementsByClassName('menu')
-    switch (lang.value){
-        case 'en':
-            menu[0].value='about us'
-    }
-}
